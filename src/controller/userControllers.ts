1bc6cfa7c2252128cbfacb0b917f805ef0b1b1b9
GenkiOsawa <taiiku39@gmail.com>
2021-05-03 00:17:55 +0900

    prisma導入

--- src/controller/userControllers.ts
+++ src/controller/userControllers.ts
@@ -1,11 +1,19 @@
 import express from 'express'
+import userRepository from '../Repository/userRepository'
 
 export default {
-  index: (
+  index: async (
     req: express.Request,
     res: express.Response,
     next: express.NextFunction
   ) => {
-    
+    try {
+      const users = await userRepository.index()
+      res.status(200).send({ status: 200, data: users })
+    } catch(e) {
+      console.error(e) // とりあえずconsoleエラー
+      next()
+    }
+    // res.send('test')
   }
 }
