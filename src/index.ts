02b391311541a9b78a3bf06bc4001812806f1689
GenkiOsawa <taiiku39@gmail.com>
2021-05-09 23:35:56 +0900

    とりあえずgraphql入れた

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import userRoutes from './routes/userRoutes'
 import dotenv from 'dotenv'
 import path from 'path'
+import { ApolloServer, gql } from 'apollo-server-express'
 
 const app = express()
 const nodeEnv = process.env.NODE_ENV
@@ -9,15 +10,26 @@ dotenv.config({ path: path.resolve(__dirname, `./config/.env.${nodeEnv}`)})
 
 const PORT = process.env.PORT || 9090
 
-app.get('/', (req, res, next) => {
-    res.status(200).send({ data: 'hoge' })
-})
-
 app.use('/users', userRoutes)
 
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!!',
+    }
+}
+
+const graphQLServer = new ApolloServer({ typeDefs, resolvers });
+graphQLServer.applyMiddleware({ app })
 
 const server = app.listen(PORT, () => {
     console.log(`🚀Runnning http://localhost:${PORT}`)
+    console.log(`🚀Running GraphQLServer http://localhost:${PORT}/graphql`)
 })
 
 export { app, server }
